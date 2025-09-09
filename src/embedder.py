1c47dd65a93d04566d76edb06c40a2b4f89a750f
venkatktp157 <vatsal157@gmail.com>
2025-09-09 18:18:11 +0530

    debugging depracated huggingface lib

--- src/embedder.py
+++ src/embedder.py
@@ -1,4 +1,4 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 
